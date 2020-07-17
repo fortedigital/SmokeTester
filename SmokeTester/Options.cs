f31ac8c5f84b45171dce1668010fc0f53b54c638
Zbigniew Winiarski <zbigniew.winiarski@fortedigital.no>
2020-07-17 13:53:33 +0200

    Revert "Removes AngleSharp and CommandLineParser"
    This reverts commit 3757edc624cd6cc350a565e9bfed955d96119cae.

--- SmokeTester/Options.cs
+++ SmokeTester/Options.cs
@@ -1,62 +1,67 @@
-﻿namespace Forte.SmokeTester
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Forte.SmokeTester
 {
     internal class Options
     {
-//        [Option('u', "url", Required = true, HelpText = "Start urls where the crawling will begin. You can pass sitemap url as well For multiple urls separate url with space. E.g. -u https://bbc.co.uk https://msn.com")]
-//        public IEnumerable<string> StartUrls { get; set; }
-//
-//        [Option('e', "test-external", Default = false, HelpText = "Test 1st level external urls.")]
-//        public bool TestExternalUrls { get; set; }
-//
-//        [Option('d', "depth", Default = 3, HelpText = "Maximum depth of url to extract.")]
-//        public int MaxDepth { get; set; }
-//
-//        [Option("maxErrors", Default = 100, HelpText = "Number of errors after which the crawler is stopped.")]
-//        public int MaxErrors { get; set; }
-//
-//        [Option('w', "workers", Default = 3, HelpText = "Number of workers.")]
-//        public int NumberOfWorkers { get; set; }
-//
-//        [Option('t', "timeout", Default = null, HelpText = "Request timeout in seconds.")]
-//        public int? RequestTimeout { get; set; }
-//
-//        [Option("maxUrls", Default = 1000, HelpText = "Number of urls after which the crawler is stopped.")]
-//        public int MaxUrls { get; set; }
-//
-//        [Option("minUrls", Default = 1, HelpText = "Minimum number of urls that is expected.")]
-//        public int MinUrls { get; set; }
-//
-//        [Option('r',"retries", Default = 0, HelpText = "Maximum number of retries (use '0' for no retry).")]
-//        public int MaxRetries { get; set; }
-//
-//        [Option("no-robots", HelpText = "Set this flag to disable parsing robots.txt in order to search for sitemaps")]
-//        public bool NoRobots { get; set; }
-//
-//        [Option('h', "headers", HelpText = "HTTP headers sent with request. Format: header_name:header_value|header_name2:header_value2.")]
-//        public string RequestHeadersRawValue { get; set; }
-//
-//        [Option('a', "user-agent", HelpText = "Sets user agent header value")]
-//        public string UserAgent { get; set; }
-//
-//        [Option( "full-summary", HelpText = "Shows full report of crawled urls")]
-//        public bool FullSummary { get; set; }
+        [Option('u', "url", Required = true, HelpText = "Start urls where the crawling will begin. You can pass sitemap url as well For multiple urls separate url with space. E.g. -u https://bbc.co.uk https://msn.com")]
+        public IEnumerable<string> StartUrls { get; set; }
 
-//        public IReadOnlyDictionary<string, string> RequestHeaders
-//        {
-//            get
-//            {
-//                if (string.IsNullOrWhiteSpace(this.RequestHeadersRawValue))
-//                {
-//                    return new Dictionary<string, string>();
-//                }
-//
-//                return this.RequestHeadersRawValue
-//                    .Split(new[] {'|', '\r', '\n'}, StringSplitOptions.RemoveEmptyEntries)
-//                    .Select(x => x.Trim())
-//                    .Where(x => string.IsNullOrEmpty(x) == false)
-//                    .Select(x => x.Split(new []{':'}, 2))
-//                    .ToDictionary(x => x.ElementAt(0), x => x.ElementAtOrDefault(1));
-//            }
-//        }
+        [Option('e', "test-external", Default = false, HelpText = "Test 1st level external urls.")]
+        public bool TestExternalUrls { get; set; }
+
+        [Option('d', "depth", Default = 3, HelpText = "Maximum depth of url to extract.")]
+        public int MaxDepth { get; set; }
+
+        [Option("maxErrors", Default = 100, HelpText = "Number of errors after which the crawler is stopped.")]
+        public int MaxErrors { get; set; }
+
+        [Option('w', "workers", Default = 3, HelpText = "Number of workers.")]
+        public int NumberOfWorkers { get; set; }
+
+        [Option('t', "timeout", Default = null, HelpText = "Request timeout in seconds.")]
+        public int? RequestTimeout { get; set; }
+
+        [Option("maxUrls", Default = 1000, HelpText = "Number of urls after which the crawler is stopped.")]
+        public int MaxUrls { get; set; }
+
+        [Option("minUrls", Default = 1, HelpText = "Minimum number of urls that is expected.")]
+        public int MinUrls { get; set; }
+
+        [Option('r',"retries", Default = 0, HelpText = "Maximum number of retries (use '0' for no retry).")]
+        public int MaxRetries { get; set; }
+
+        [Option("no-robots", HelpText = "Set this flag to disable parsing robots.txt in order to search for sitemaps")]
+        public bool NoRobots { get; set; }
+
+        [Option('h', "headers", HelpText = "HTTP headers sent with request. Format: header_name:header_value|header_name2:header_value2.")]
+        public string RequestHeadersRawValue { get; set; }
+
+        [Option('a', "user-agent", HelpText = "Sets user agent header value")]
+        public string UserAgent { get; set; }
+
+        [Option( "full-summary", HelpText = "Shows full report of crawled urls")]
+        public bool FullSummary { get; set; }
+
+        public IReadOnlyDictionary<string, string> RequestHeaders
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(this.RequestHeadersRawValue))
+                {
+                    return new Dictionary<string, string>();
+                }
+
+                return this.RequestHeadersRawValue
+                    .Split(new[] {'|', '\r', '\n'}, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => x.Trim())
+                    .Where(x => string.IsNullOrEmpty(x) == false)
+                    .Select(x => x.Split(new []{':'}, 2))
+                    .ToDictionary(x => x.ElementAt(0), x => x.ElementAtOrDefault(1));
+            }
+        }
     }
 }
--- SmokeTester/Program.cs
+++ SmokeTester/Program.cs
@@ -1,109 +1,115 @@
-﻿namespace Forte.SmokeTester
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Forte.SmokeTester.Extractor;
+
+namespace Forte.SmokeTester
 {
     public static class Program
     {
         public static int Main(string[] args)
         {
-            return 0;
-//            return CommandLine.Parser.Default
-//                .ParseArguments<Options>(args)
-//                .MapResult(Run, errs => 1);
+            return CommandLine.Parser.Default
+                .ParseArguments<Options>(args)
+                .MapResult(Run, errs => 1);
         }
 
-//        private static int Run(Options opts)
-//        {
-//            var cancellationTokenSource = new CancellationTokenSource();
-//            var observer = new CrawlerObserver(
-//                cancellationTokenSource,
-//                opts.MaxErrors,
-//                opts.MaxUrls);
-//
-//            var crawler = CreateCrawler(opts, observer);
-//            crawler.Enqueue(opts.StartUrls.Select(x => new Uri(x)));
-//
-//            var parseRobots = opts.NoRobots == false;
-//            if (parseRobots)
-//            {
-//                var rootUrls = opts.StartUrls.Select(x => new Uri(x).GetLeftPart(UriPartial.Authority));
-//                var robotsTxtUrls = rootUrls.Select(x => new Uri(new Uri(x), "/robots.txt")).Distinct();
-//                crawler.Enqueue(robotsTxtUrls);
-//            }
-//
-//            Console.CancelKeyPress += (sender, eventArgs) =>
-//            {
-//                eventArgs.Cancel = true;
-//                cancellationTokenSource.Cancel();
-//            };
-//
-//            var result = crawler.Crawl(cancellationTokenSource.Token).Result;
-//
-//            WriteSummary(result, observer, opts.FullSummary);
-//            
-//            var crawledUrlsCount = observer.CrawledUrls.Count;
-//            if (crawledUrlsCount < opts.MinUrls)
-//            {
-//                Console.WriteLine($"\nExpected at least {opts.MinUrls} urls but crawled only {crawledUrlsCount}.");
-//                return 2;
-//            }
-//
-//            return observer.Errors.Count > 0 ? 1 : 0;
-//        }
-//
-//        private static Crawler CreateCrawler(Options opts, ICrawlerObserver observer)
-//        {
-//            var startUrlAuthorities = opts.StartUrls.Select(x => new Uri(x).Authority);
-//
-//            var linkExtractor = new CompositeExtractor(new HtmlLinkExtractor(), new SiteMapLinkExtractor(), new RobotsTxtSitemapExtractor());
-//            var crawlRequestFilter = new CompositeFilter(
-//                new AuthorityFilter(startUrlAuthorities, opts.TestExternalUrls),
-//                new MaxDepthFilter(opts.MaxDepth));
-//
-//            var requestTimeout = opts.RequestTimeout != null
-//                ? (TimeSpan?)TimeSpan.FromSeconds(opts.RequestTimeout.Value)
-//                : null;
-//
-//            return new Crawler(
-//                new WorkerPool(opts.NumberOfWorkers),
-//                crawlRequestFilter,
-//                linkExtractor,
-//                observer,
-//                opts.RequestHeaders,
-//                requestTimeout,
-//                opts.MaxRetries,
-//                opts.UserAgent);
-//        }
-//
-//        private static void WriteSummary(IReadOnlyDictionary<Uri, CrawledUrlProperties> result, CrawlerObserver observer, bool fullSummary)
-//        {
-//            Console.WriteLine($"\nDiscovered urls: {result.Count}\nCrawled urls: {observer.CrawledUrls.Count}\nCrawl warnings: {observer.Warnings.Count}\nCrawl errors: {observer.Errors.Count}");
-//
-//            if (observer.Warnings.Count > 0)
-//            {
-//                Console.WriteLine("\nCrawl warnings:\n");
-//                foreach (var error in observer.Warnings)
-//                {
-//                    Console.WriteLine($"{error.Status}: {error.Url}\nReferrers:\n  {string.Join("\n  ", result[error.Url].Referrers)}\n");
-//                }
-//            }
-//
-//            if (observer.Errors.Count > 0)
-//            {
-//                Console.WriteLine("\nCrawl errors:\n");
-//                foreach (var error in observer.Errors)
-//                {
-//                    Console.WriteLine($"{error.Exception?.FlattenInnerMessages() ?? error.Status.ToString()}: {error.Url}\nReferrers:\n  {string.Join("\n  ", result[error.Url].Referrers)}\n");
-//                }
-//            }
-//
-//            if (fullSummary)
-//            {
-//                Console.WriteLine("###########################################################################################33");
-//                foreach (var uri in result.Where(x => x.Value.Status != null).Select(x => x.Key).OrderBy(x => x.ToString()))
-//                {
-//                    var entry = result[uri];
-//                    Console.WriteLine($"[{entry.Status}] {uri}: \nReferrers:\n  {string.Join("\n  ", entry.Referrers)}\n");
-//                }
-//            }
-//        }
+        private static int Run(Options opts)
+        {
+            var cancellationTokenSource = new CancellationTokenSource();
+            var observer = new CrawlerObserver(
+                cancellationTokenSource,
+                opts.MaxErrors,
+                opts.MaxUrls);
+
+            var crawler = CreateCrawler(opts, observer);
+            crawler.Enqueue(opts.StartUrls.Select(x => new Uri(x)));
+
+            var parseRobots = opts.NoRobots == false;
+            if (parseRobots)
+            {
+                var rootUrls = opts.StartUrls.Select(x => new Uri(x).GetLeftPart(UriPartial.Authority));
+                var robotsTxtUrls = rootUrls.Select(x => new Uri(new Uri(x), "/robots.txt")).Distinct();
+                crawler.Enqueue(robotsTxtUrls);
+            }
+
+            Console.CancelKeyPress += (sender, eventArgs) =>
+            {
+                eventArgs.Cancel = true;
+                cancellationTokenSource.Cancel();
+            };
+
+            var result = crawler.Crawl(cancellationTokenSource.Token).Result;
+
+            WriteSummary(result, observer, opts.FullSummary);
+            
+            var crawledUrlsCount = observer.CrawledUrls.Count;
+            if (crawledUrlsCount < opts.MinUrls)
+            {
+                Console.WriteLine($"\nExpected at least {opts.MinUrls} urls but crawled only {crawledUrlsCount}.");
+                return 2;
+            }
+
+            return observer.Errors.Count > 0 ? 1 : 0;
+        }
+
+        private static Crawler CreateCrawler(Options opts, ICrawlerObserver observer)
+        {
+            var startUrlAuthorities = opts.StartUrls.Select(x => new Uri(x).Authority);
+
+            var linkExtractor = new CompositeExtractor(new HtmlLinkExtractor(), new SiteMapLinkExtractor(), new RobotsTxtSitemapExtractor());
+            var crawlRequestFilter = new CompositeFilter(
+                new AuthorityFilter(startUrlAuthorities, opts.TestExternalUrls),
+                new MaxDepthFilter(opts.MaxDepth));
+
+            var requestTimeout = opts.RequestTimeout != null
+                ? (TimeSpan?)TimeSpan.FromSeconds(opts.RequestTimeout.Value)
+                : null;
+
+            return new Crawler(
+                new WorkerPool(opts.NumberOfWorkers),
+                crawlRequestFilter,
+                linkExtractor,
+                observer,
+                opts.RequestHeaders,
+                requestTimeout,
+                opts.MaxRetries,
+                opts.UserAgent);
+        }
+
+        private static void WriteSummary(IReadOnlyDictionary<Uri, CrawledUrlProperties> result, CrawlerObserver observer, bool fullSummary)
+        {
+            Console.WriteLine($"\nDiscovered urls: {result.Count}\nCrawled urls: {observer.CrawledUrls.Count}\nCrawl warnings: {observer.Warnings.Count}\nCrawl errors: {observer.Errors.Count}");
+
+            if (observer.Warnings.Count > 0)
+            {
+                Console.WriteLine("\nCrawl warnings:\n");
+                foreach (var error in observer.Warnings)
+                {
+                    Console.WriteLine($"{error.Status}: {error.Url}\nReferrers:\n  {string.Join("\n  ", result[error.Url].Referrers)}\n");
+                }
+            }
+
+            if (observer.Errors.Count > 0)
+            {
+                Console.WriteLine("\nCrawl errors:\n");
+                foreach (var error in observer.Errors)
+                {
+                    Console.WriteLine($"{error.Exception?.FlattenInnerMessages() ?? error.Status.ToString()}: {error.Url}\nReferrers:\n  {string.Join("\n  ", result[error.Url].Referrers)}\n");
+                }
+            }
+
+            if (fullSummary)
+            {
+                Console.WriteLine("###########################################################################################33");
+                foreach (var uri in result.Where(x => x.Value.Status != null).Select(x => x.Key).OrderBy(x => x.ToString()))
+                {
+                    var entry = result[uri];
+                    Console.WriteLine($"[{entry.Status}] {uri}: \nReferrers:\n  {string.Join("\n  ", entry.Referrers)}\n");
+                }
+            }
+        }
     }
 }
